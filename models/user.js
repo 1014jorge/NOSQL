0e0b4b667a263da1588864708a319203f7d35e1b
Jorge Gonzalez <jorge.1014@gmail.com>
2023-11-11 01:05:24 -0800

    Updated user, and index under models

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require ("mongoose");
+
+const userSchema = new Schema( {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.|\..+/, "Must match and email address"]
+
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+})
