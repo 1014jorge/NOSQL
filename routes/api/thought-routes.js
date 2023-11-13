c3af56e77e0bd428255788fd8fe59f0b2207b1aa
Jorge Gonzalez <jorge.1014@gmail.com>
2023-11-12 22:03:53 -0800

    updated routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -27,13 +27,55 @@ router.post("/", async (req, res) => {
             return res.status(404).json({ message: `Thought created but no user with this ID` });
         }
 
-        
+
         res.status(200).json({ ...dbThoughtData, message: `Thought successfully created` });
     } catch (err) {
         res.status(404).json(err)
     }
 })
 
+router.get("/:thoughtId", async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: `No thought with this ID` })
+        }
+        res.status(200).json(dbThoughtData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
+router.put("/:thoughtId", async (req, res) => {
+    try {
+        const dbThoughtData = Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        );
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: "No thought with this ID" })
+        }
+        res.json(dbThoughtData);
 
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
+router.delete("/:thoughtId/reactions/:reactionId", async (req, res) => {
+
+    try {
+    } catch (err) {
+
+    }
+})
+
+router.post("/:thoughtId/reactions", async (req, res) => {
+    try {
+    } catch (err) {
+
+    }
+
+})
 module.exports = router;
