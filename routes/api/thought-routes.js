6197ea23e551ac4c32c61a9205c1f50042bd0532
Jorge Gonzalez <jorge.1014@gmail.com>
2023-12-03 21:43:22 -0800

    updated index, thought-routes and user routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,117 +1,3 @@
-// const router = require("express").Router();
-// const { json } = require("express");
-// const { User, Thought } = require("../../models");
-// const { db } = require("../../models/User");
-
-
-// //api thoughts
-
-// router.get("/", async (req, res) => {
-//     try {
-//         const dbThoughtData = await Thought.find().sort({ createdAt: -1 })
-//         res.status(200).json(dbThoughtData);
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-
-// });
-
-// router.post("/", async (req, res) => {
-//     try {
-//         const dbThoughtData = await Thought.create(req.body);
-//         const dbUserData = await User.findOneAndUpdate({
-//             _id: req.body.userId
-//         },
-//             { $push: { thoughts: dbThoughtData._id } },
-//             { new: true }
-//         );
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: "Thought created but no user with this ID" });
-//         }
-//         res.status(200).json({ message: "Thought successfully created" });
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
-
-// router.get("/:thoughtId", async (req, res) => {
-//     try {
-//         const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
-//         if (!dbThoughtData) {
-//             return res.status(404).json({ message: "No thought with this ID" });
-//         }
-//         res.status(200).json(dbThoughtData)
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
-// router.put("/:thoughtId", async (req, res) => {
-//     try {
-//         const dbThoughtData = Thought.findOneAndUpdate(
-//             { _id: req.params.thoughtId },
-//             { $set: req.body },
-//             { runValidators: true, new: true }
-//         );
-//         if (!dbThoughtData) {
-//             return res.status(404).json({ message: "No thought with this ID" });
-//         }
-//         res.status(200).json(dbThoughtData);
-
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-
-// });
-// router.delete("/:thoughtId", async (req, res) => {
-//     try {
-//         const dbThoughtData = Thought.findOneAndRemove({ _id: req.params.thoughtId });
-//         if (!dbThoughtData) {
-//             return res.status(404).json({ message: "No thought with this ID" });
-//         }
-//         const dbUserData = await User.findOneAndUpdate({ thoughts: req.params.thoughtId },
-//             { $pull: { thoughts: req.params.thoughtId } },
-//             { new: true });
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: "No thought with this ID" })
-//         }
-//         res.json({ message: "Thought Succesfully deleted" })
-//     } catch (err) {
-//         res.json(err)
-//     }
-// });
-
-// router.post("/:thoughtId/reactions", async (req, res) => {
-//     try {
-//         const dbThoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId},
-//             {$addToSet: {reactions: req.body}},
-//             {runValidators: true, new: true});
-//             if(!dbThoughtData){
-//                 return res.status(404).json({ message: "No thought with this ID"});
-//             }
-//             res.status(200).json(dbThoughtData);
-//     } catch (err) {
-//         res.status(500).json(err);
-
-//     }
-
-// });
-// router.delete("/:thoughtId/reactions/:reactionId", async (req, res) => {
-//     try {
-//         const dbThoughtData = await Thought.findOneAndUpdate({_id: req.params.thoughtId},
-//             { $pull: { reactions: {reactionId: req.params.reactionId}}},
-//             { runValidators: true, new: true });
-//             if (!dbThoughtData) {
-//                 return res.status(404).json({ message: "No thought with this ID"});
-//             }
-//             res.status(200),json(dbThoughtData);
-//     } catch (err){
-//         res.status(500),json(err);
-//     }
-// });
-
-// module.exports = router;
-
 const router = require("express").Router();
 const {
     getAllThought,
@@ -123,20 +9,20 @@ const {
     removeReaction,
 } = require("../../controllers/thought-controller");
 
-// /api/thoughts
+//api/thoughts
 router.route("/").get(getAllThought).post(createThought);
 
-// /api/thoughts/:id
+//api/thoughts/:id
 router
     .route("/:id")
     .get(getThoughtById)
     .put(updateThought)
     .delete(deleteThought);
 
-// /api/thoughts/:thoughtId/reactions
+//api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions").post(addReaction);
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
+//api/thoughts/:thoughtId/reactions/:reactionId
 router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,122 +1,3 @@
-// const router = require("express").Router();
-// const { User, Thought } = require("../../models");
-// const { findOneAndDelete } = require("../../models/User");
-
-// //api/gets all users
-// router.get("/", async (req, res) => {
-//     try {
-//         const dbUserData = await User.find().select("-__v")
-//         res.status(200).json(dbUserData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
-// //creates new users
-// router.post("/", async (req, res) => {
-//     try {
-//         const dbUserData = await User.create(req.body);
-//         res.status(200).json(dbUserData);
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
-
-// //api/users/:userid
-// router.get("/:userId", async (req, res) => {
-//     try {
-//         const dbUserData = await User.findOne({ _id: req.params.userId })
-//             .select("-__v")
-//             .populate("friends")
-//             .populate("thoughts");
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: "No user with this ID" })
-//         }
-//         res.status(200).json(dbUserData);
-//     } catch (err) {
-//         res.status(500).json(err)
-
-//     }
-// });
-
-// router.put("/:userId", async (req, res) => {
-//     try {
-//         const dbUserData = await User.findOneAndUpdate({
-//             _id: req.params.userId,
-//         }, {
-//             $set: req.body,
-//         },
-//             {
-//                 runValidators: true,
-//                 new: true
-
-//             });
-
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: "No user with this ID" })
-//         }
-//         res.status(200).json(dbUserData)
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
-
-// router.delete("/:userId", async (req, res) => {
-//     try {
-//         const dbUserData = await User.findOneAndDelete({ _id: req.params.userId })
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: "No user with this ID" })
-//         }
-//         await Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
-
-//         res.status(200).json({ message: "User and associated thoughts deleted" })
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
-
-
-
-// //api/users/friends
-
-// router.post("/:userId/friends/:friendId", async (req, res) => {
-//     try {
-//         const dbUserData = User.findOneAndUpdate(
-//             { _id: req.params.userId },
-//             {
-//                 $addToSet: { friends: req.params.friendId },
-//             },
-//             {
-//                 new: true
-//             }
-//         );
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: `No user with this ID` })
-//         }
-//         res.status(200).json(dbUserData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
-
-
-// router.delete("/:userId/friends/:friendId", async (req, res) => {
-//     try {
-//         const dbUserData = User.findOneAndDelete(
-//             { _id: req.params.userId },
-//             { $pull: { friends: req.params.friendId } },
-//             { new: true }
-//         );
-//         if (!dbUserData) {
-//             return res.status(404).json({ message: "No user with this ID" })
-//         }
-//         res.status(200).json(dbUserData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// })
-
-// module.exports = router;
-
 const router = require("express").Router();
 const {
   getAllUser,
@@ -128,13 +9,13 @@ const {
   removeFriend,
 } = require("../../controllers/user-controller");
 
-// /api/users
+//api/users
 router.route("/").get(getAllUser).post(createUser);
 
-// /api/users/:id
+//api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/friends/:friendId
+//api/users/:userId/friends/:friendId
 router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
